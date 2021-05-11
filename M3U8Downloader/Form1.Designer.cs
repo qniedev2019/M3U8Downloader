75cec653a522a53821c7f5970ef58afbd2948ae4
qniedev2019@outlook.com <qniedev2019@outlook.com>
2021-05-11 16:35:53 +0700

    Add project files.

---
+++ M3U8Downloader/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace M3U8Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.bgDownload = new System.ComponentModel.BackgroundWorker();
+            this.cbbSource = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rtb1 = new System.Windows.Forms.RichTextBox();
+            this.lblProcess = new System.Windows.Forms.Label();
+            this.txtM3U8 = new System.Windows.Forms.TextBox();
+            this.lblM3U8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL Link:";
+            // 
+            // txtURL
+            // 
+            this.txtURL.BackColor = System.Drawing.Color.White;
+            this.txtURL.Font = new System.Drawing.Font("Segoe UI", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtURL.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtURL.Location = new System.Drawing.Point(27, 138);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(444, 23);
+            this.txtURL.TabIndex = 1;
+            this.txtURL.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtURL_MouseClick);
+            this.txtURL.TextChanged += new System.EventHandler(this.txtURL_TextChanged);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnDownload.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDownload.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDownload.Font = new System.Drawing.Font("Segoe UI", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDownload.ForeColor = System.Drawing.Color.Transparent;
+            this.btnDownload.Location = new System.Drawing.Point(27, 255);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(216, 33);
+            this.btnDownload.TabIndex = 4;
+            this.btnDownload.Text = "DOWNLOAD";
+            this.btnDownload.UseVisualStyleBackColor = false;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.Transparent;
+            this.btnExit.Location = new System.Drawing.Point(255, 255);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(216, 33);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(190, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "M3U8 DOWNLOADER";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(203, 42);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(102, 13);
+            this.lblStatus.TabIndex = 7;
+            this.lblStatus.Text = "(VNExprees, IONE, ...)";
+            // 
+            // bgDownload
+            // 
+            this.bgDownload.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgDownload_DoWork);
+            this.bgDownload.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgDownload_RunWorkerCompleted);
+            // 
+            // cbbSource
+            // 
+            this.cbbSource.BackColor = System.Drawing.Color.White;
+            this.cbbSource.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cbbSource.FormattingEnabled = true;
+            this.cbbSource.Items.AddRange(new object[] {
+            "VTV.VN",
+            "KENH14.VN",
+            "VNEXPRESS.VN",
+            "IONE.VNEXPRESS.VN",
+            "SHOPEE",
+            "CUSTOM"});
+            this.cbbSource.Location = new System.Drawing.Point(27, 83);
+            this.cbbSource.Name = "cbbSource";
+            this.cbbSource.Size = new System.Drawing.Size(444, 25);
+            this.cbbSource.TabIndex = 0;
+            this.cbbSource.TextChanged += new System.EventHandler(this.cbbSource_TextChanged);
+            this.cbbSource.Click += new System.EventHandler(this.cbbSource_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Select source:";
+            // 
+            // rtb1
+            // 
+            this.rtb1.BackColor = System.Drawing.Color.White;
+            this.rtb1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtb1.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtb1.ForeColor = System.Drawing.Color.DimGray;
+            this.rtb1.Location = new System.Drawing.Point(455, 2);
+            this.rtb1.Name = "rtb1";
+            this.rtb1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.rtb1.Size = new System.Drawing.Size(45, 23);
+            this.rtb1.TabIndex = 3;
+            this.rtb1.Text = "rtb";
+            this.rtb1.Visible = false;
+            // 
+            // lblProcess
+            // 
+            this.lblProcess.AutoSize = true;
+            this.lblProcess.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblProcess.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblProcess.Font = new System.Drawing.Font("Segoe UI", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProcess.ForeColor = System.Drawing.Color.Blue;
+            this.lblProcess.Location = new System.Drawing.Point(26, 235);
+            this.lblProcess.Name = "lblProcess";
+            this.lblProcess.Size = new System.Drawing.Size(69, 17);
+            this.lblProcess.TabIndex = 11;
+            this.lblProcess.Text = "Processing:";
+            this.lblProcess.Visible = false;
+            this.lblProcess.Click += new System.EventHandler(this.lblProcess_Click);
+            // 
+            // txtM3U8
+            // 
+            this.txtM3U8.BackColor = System.Drawing.Color.White;
+            this.txtM3U8.Font = new System.Drawing.Font("Segoe UI", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtM3U8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtM3U8.Location = new System.Drawing.Point(29, 191);
+            this.txtM3U8.Name = "txtM3U8";
+            this.txtM3U8.Size = new System.Drawing.Size(444, 23);
+            this.txtM3U8.TabIndex = 13;
+            // 
+            // lblM3U8
+            // 
+            this.lblM3U8.AutoSize = true;
+            this.lblM3U8.Location = new System.Drawing.Point(26, 171);
+            this.lblM3U8.Name = "lblM3U8";
+            this.lblM3U8.Size = new System.Drawing.Size(72, 17);
+            this.lblM3U8.TabIndex = 12;
+            this.lblM3U8.Text = "M3U8 Link:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(503, 310);
+            this.Controls.Add(this.txtM3U8);
+            this.Controls.Add(this.lblM3U8);
+            this.Controls.Add(this.lblProcess);
+            this.Controls.Add(this.rtb1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbbSource);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(519, 349);
+            this.MinimumSize = new System.Drawing.Size(519, 349);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "U3M8 Downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblStatus;
+        private System.ComponentModel.BackgroundWorker bgDownload;
+        private System.Windows.Forms.ComboBox cbbSource;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox rtb1;
+        private System.Windows.Forms.Label lblProcess;
+        private System.Windows.Forms.TextBox txtM3U8;
+        private System.Windows.Forms.Label lblM3U8;
+    }
+}
+
---
+++ M3U8Downloader/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.IO;
+using System.Threading;
+
+namespace M3U8Downloader
+{
+    public partial class Form1 : Form
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr window, int message, int wparam, int lparam);
+
+        private const int SbBottom = 0x7;
+        private const int WmVscroll = 0x115;
+
+        public string URLlink { get; private set; }
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        string str_Source, cmd_Source = "";
+        string str_vtv, str_kenh14, str_vnexpress, str_shopee, str_custom = "";
+        string Referer, m3u8, Title = "";
+        string DownloadFolder, DownloadFile = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public static string getBetween(string strSource, string strStart, string strEnd)
+        {
+            const int kNotFound = -1;
+
+            var startIdx = strSource.IndexOf(strStart);
+            if (startIdx != kNotFound)
+            {
+                startIdx += strStart.Length;
+                var endIdx = strSource.IndexOf(strEnd, startIdx);
+                if (endIdx > startIdx)
+                {
+                    return strSource.Substring(startIdx, endIdx - startIdx);
+                }
+            }
+            return String.Empty;
+        }
+
+        public void ExecuteCommand(string command)
+        {
+
+            //Create process
+            System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+            pProcess.StartInfo.FileName = "CMD.exe";
+            pProcess.StartInfo.Arguments = command;
+            pProcess.StartInfo.CreateNoWindow = true;
+            pProcess.StartInfo.UseShellExecute = false;
+            pProcess.StartInfo.RedirectStandardOutput = true;
+            // Set event handler
+            pProcess.OutputDataReceived += new DataReceivedEventHandler(SortOutputHandler);
+            pProcess.Start();
+
+            // Start the asynchronous read
+            pProcess.BeginOutputReadLine();
+
+            pProcess.WaitForExit();
+            pProcess.Close();
+        }
+
+        void SortOutputHandler(object sender, DataReceivedEventArgs e)
+        {
+            Trace.WriteLine(e.Data);
+            this.BeginInvoke(new MethodInvoker(() =>
+            {
+                rtb1.AppendText(e.Data + Environment.NewLine ?? string.Empty);
+                SendMessage(rtb1.Handle, WmVscroll, SbBottom, 0x0);
+            }));
+        }
+
+        private void cbbSource_Click(object sender, EventArgs e)
+        {
+            cbbSource.DroppedDown = true;
+            lblProcess.Visible = false;
+        }
+
+        private void txtURL_TextChanged(object sender, EventArgs e)
+        {
+            lblProcess.Visible = false;
+        }
+
+        private void lblProcess_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", DownloadFile);
+        }
+
+        private void txtURL_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtURL.SelectAll();
+        }
+
+        private void txtM3U8_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            if(cbbSource.Text == "")
+            {
+                MessageBox.Show("Please select Source first.", "M3U8 Downloader", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                //widthStart = "";
+                //widthEnd = "";
+                //site = cbbSource.Text;
+                //url = txtURL.Text;
+
+                //switch (site)
+                //{
+                //    case "VNEXPRESS.VN":
+                //        widthStart = "contentUrl\": \"";
+                //        widthEnd = "master.m3u8\"";
+                //        break;
+
+                //}
+
+                ////Get Source
+
+                //var html = txtURL.Text;
+                //HtmlWeb web = new HtmlWeb();
+                //var htmlDoc = web.Load(html);
+                //var node = htmlDoc.DocumentNode.SelectSingleNode("//head");
+
+                //rtb1.Text = ("Node Name: " + node.Name + "\n" + node.OuterHtml);
+
+                //totalLines = rtb1.Lines.Length;
+                //foreach (var line in rtb1.Lines)
+                //{
+
+                //    if (line.Contains(widthEnd))
+                //    {
+                //        data = getBetween(line, widthStart, widthEnd);
+                //        m3u8 = data + "master.m3u8";
+                //    }
+
+                //}
+                rtb1.Text = "";
+                btnDownload.Text = "Downloading...";
+                bgDownload.RunWorkerAsync();
+            }
+            
+        }
+
+        public void reSoure()
+        {
+
+
+            //
+            m3u8 = txtM3U8.Text;
+            Referer = txtURL.Text;
+            Title = "Video_" + DateTime.Now.ToString("yyyyddMM_hhmmss");
+            DownloadFolder = Environment.CurrentDirectory.ToString();
+            DownloadFile = DownloadFolder + @"\" + Title + ".mp4";
+            // Library
+            // VTV: ffmpeg -user_agent "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us)AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341Safari/528.16" -referer "https://vtv.vn/video/thoi-su-19h-vtv1-26-6-2018-307696.htm" -i "https://hls.mediacdn.vn/vtv/2018/6/26/2606thoisu19h-1530017484872626935899-fc08f.mp4/1080.m3u8?v=626c23d3" -c copy "test.mp4"
+
+            // Kenh14: ffmpeg -user_agent "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us)AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341Safari/528.16" -referer "https://kenh14.vn/bich-phuong-toi-do-tat-ca-nhung-ai-cua-minh-nguoi-yeu-cu-nhan-tin-moi-di-dam-cuoi-toi-khong-seen-20200603225408402.chn" -i "https://hls.mediacdn.vn/kenh14/2020/6/3/cBhl3NXTf60-1591202288037689890771.mp4/master.m3u8" -c copy "test.mp4"
+
+            // VNEXPRESS: ffmpeg -headers "Referer: https://ione.vnexpress.net/tin-tuc/sao/chau-a/fan-dao-lai-video-chu-tich-jyp-dan-mat-hee-chul-4036216.htm" -i https://d1.vnecdn.net/ione/video/video/web/mp4//2020/01/02/fan-dao-lai-video-chu-tich-jyp-dan-mat-hee-chul-1577949258/index-v1-a1.m3u8 -c copy -bsf:a aac_adtstoasc OUTPUT.MP4
+
+            // SHOPEE: youtube-dl -f bestvideo+bestaudio https://cv.shopee.vn/dash/2019/11/19/18/,J3MCkOS31JaAdKGC_120.mp4,J3MCkOS31JaAdKGC.mp4,.urlset/manifest.mpd
+
+            str_vtv = "/C ffmpeg -user_agent \"Mozilla / 5.0(iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en - us)AppleWebKit / 528.18(KHTML, like Gecko) Version / 4.0 Mobile / 7A341Safari / 528.16\" -referer \"" + Referer + "\" -i \"" + m3u8 + "\" -c copy \"" + Title + ".mp4\"";
+
+            str_kenh14 = "/C ffmpeg -user_agent \"Mozilla / 5.0(iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en - us)AppleWebKit / 528.18(KHTML, like Gecko) Version / 4.0 Mobile / 7A341Safari / 528.16\" -referer \"" + Referer + "\" -i \"" + m3u8 + "\" -c copy \"" + Title + ".mp4\"";
+
+            str_vnexpress = "/C ffmpeg -headers \"Referer: " + Referer + "\" -i \"" + m3u8 + "\" -c copy -bsf:a aac_adtstoasc " + Title + ".mp4";
+
+            str_shopee = "/C youtube-dl -f bestvideo+bestaudio " + m3u8 + ""; // m3u8 manifest.mpd
+
+            str_custom = "/C ffmpeg -headers \"Referer: " + Referer + "\" -i " + m3u8 + " -c copy -bsf:a aac_adtstoasc " + Title + ".mp4";
+
+            switch (cmd_Source)
+            {
+                //VTV.VN
+                //KENH14.VN
+                //VNEXPRESS.VN
+                //IONE.VNEXPRESS.VN
+                //SHOPEE
+                //CUSTOM
+                case "VTV.VN":
+                    cmd_Source = str_vtv;
+                    break;
+                case "KENH14.VN":
+                    cmd_Source = str_kenh14;
+                    break;
+                case "VNEXPRESS.VN":
+                    cmd_Source = str_vnexpress;
+                    break;
+                case "IONE.VNEXPRESS.VN":
+                    cmd_Source = str_vnexpress;
+                    break;
+                case "SHOPEE":
+                    cmd_Source = str_shopee;
+                    break;
+                case "CUSTOM":
+                    cmd_Source = str_custom;
+                    break;
+            }
+            
+            
+
+        }
+        public void Download()
+        {
+            reSoure();
+            ExecuteCommand(cmd_Source);
+        }
+
+        private void bgDownload_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Download();
+        }
+
+        private void bgDownload_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lblProcess.Visible = true;
+            lblProcess.Text = Title+".mp4";
+            if (MessageBox.Show("Download completed. Do you want open folder download?", "M3U8 Downloade", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+            {
+                Process.Start("explorer.exe", DownloadFolder);
+            }
+
+            btnDownload.Text = "DOWNLOAD";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cbbSource_TextChanged(object sender, EventArgs e)
+        {
+            str_Source = cbbSource.Text;
+            switch (str_Source)
+            {
+                //VTV.VN
+                //KENH14.VN
+                //VNEXPRESS.VN
+                //IONE.VNEXPRESS.VN
+                //SHOPEE
+                //CUSTOM
+                case "VTV.VN":
+                    cmd_Source = "VTV.VN";
+                    break;
+                case "KENH14.VN":
+                    cmd_Source = "KENH14.VN";
+                    break;
+                case "VNEXPRESS.VN":
+                    cmd_Source = "VNEXPRESS.VN";
+                    break;
+                case "IONE.VNEXPRESS.VN":
+                    cmd_Source = "IONE.VNEXPRESS.VN";
+                    break;
+                case "SHOPEE":
+                    cmd_Source = "SHOPEE";
+                    break;
+                case "CUSTOM":
+                    cmd_Source = "CUSTOM";
+                    break;
+            }
+        }
+        }
+}
